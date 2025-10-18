dbfe2d05b433af5dda933f237731562d0dcc7efc
berleyjess <berleyjes@gmail.com>
2025-10-18 00:04:42 +0000

    Cambio en saxon :D

--- app.py
+++ app.py
@@ -29,17 +29,22 @@ def cadena_original():
         xslt_url = "https://www.sat.gob.mx/sitio_internet/cfd/4/cadenaoriginal_4_0/cadenaoriginal_4_0.xslt"
 
         with PySaxonProcessor(license=False) as proc:
+            xslt_response = requests.get(xslt_url)
+            xslt_content = xslt_response.content.decode('utf-8')  # ← DECODIFICAR bytes a string
             # Carga el XSLT desde la URL
             xslt_proc = proc.new_xslt30_processor()
             xslt_executable = xslt_proc.compile_stylesheet(stylesheet_text=requests.get(xslt_url).content)
 
             # Carga el XML desde los datos de la solicitud
-            xml_doc = proc.parse_xml(xml_text=xml_data.decode('utf-8'))
+            xml_content = xml_data.decode('utf-8')  # ← Asegurar que sea string
+            #xml_doc = proc.parse_xml(xml_text=xml_data.decode('utf-8'))
+            #xml_doc = proc.parse_xml(xml_text=xml_data.decode('utf-8'))
+            xml_doc = proc.parse_xml(xml_text=xml_content)
 
             # Realiza la transformación
             cadena = xslt_executable.transform_to_string(xdm_node=xml_doc)
 
-            return Response(cadena, mimetype="text/plain")
+            return Response(cadena.strip(), mimetype="text/plain")#sin .strip()
 
     except Exception as e:
         return Response("Error: " + str(e), status=500, mimetype="text/plain")
